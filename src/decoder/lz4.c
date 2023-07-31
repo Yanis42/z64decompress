cc2968705d8cb25af1ab402d374f57d8200bc975
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2023-08-01 01:34:39 +0200

    progress

--- src/decoder/lz4.c
+++ src/decoder/lz4.c
@@ -1,6 +1,7 @@
 /* adapted from libdragon */
 
 #include <assert.h>
+#include <stdint.h>
 #include <stdbool.h>
 
 #include "private.h"
@@ -8,17 +9,17 @@
 /*** General ***/
 #define FALLTHROUGH __attribute__((fallthrough))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
-#define likely(x) (x)
-#define unlikely(x) (x)
+#define likely(x)   __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#define ALIGN8(val) (((val) + 7) & ~7)
 
-typedef unsigned long long uint64_t;
 typedef signed long ssize_t;
 
 /*** Ring Buffer ***/
 #define RING_BUFFER_SIZE (16 * 1024)
 
 typedef struct decoder_ringbuf {
-	unsigned char ringbuf[RING_BUFFER_SIZE];    ///< The ring buffer itself
+	uint8_t ringbuf[RING_BUFFER_SIZE];    ///< The ring buffer itself
 	unsigned int ringbuf_pos;                   ///< Current write position in the ring buffer
 } decoder_ringbuf;
 
@@ -26,7 +27,12 @@ void __ringbuf_init(decoder_ringbuf* ringbuf) {
     ringbuf->ringbuf_pos = 0;
 }
 
-void __ringbuf_write(decoder_ringbuf* ringbuf, unsigned char* src, int count) {
+inline void __ringbuf_writebyte(decoder_ringbuf *ringbuf, uint8_t byte) {
+    ringbuf->ringbuf[ringbuf->ringbuf_pos++] = byte;
+    ringbuf->ringbuf_pos &= (RING_BUFFER_SIZE - 1);
+}
+
+void __ringbuf_write(decoder_ringbuf* ringbuf, uint8_t* src, int count) {
     while (count > 0) {
         int n = MIN(count, (int)(RING_BUFFER_SIZE - ringbuf->ringbuf_pos));
         memcpy(ringbuf->ringbuf + ringbuf->ringbuf_pos, src, n);
@@ -37,8 +43,8 @@ void __ringbuf_write(decoder_ringbuf* ringbuf, unsigned char* src, int count) {
     }
 }
 
-void __ringbuf_copy(decoder_ringbuf* ringbuf, int copy_offset, unsigned char* dst, int count) {
-    int ringbuf_copy_pos = (ringbuf->ringbuf_pos - copy_offset) & (RING_BUFFER_SIZE-1);
+void __ringbuf_copy(decoder_ringbuf* ringbuf, int copy_offset, uint8_t* dst, int count) {
+    int ringbuf_copy_pos = (ringbuf->ringbuf_pos - copy_offset) & (RING_BUFFER_SIZE - 1);
     int dst_pos = 0;
 
     while (count > 0) {
@@ -47,7 +53,7 @@ void __ringbuf_copy(decoder_ringbuf* ringbuf, int copy_offset, unsigned char* ds
 		wn = wn < (int)(RING_BUFFER_SIZE - ringbuf->ringbuf_pos) ? wn : (int)(RING_BUFFER_SIZE - ringbuf->ringbuf_pos);
         count -= wn;
 
-        // Check if there's an overlap in the ring buffer between read and write pos, in which
+        // Check if there's an overlap in the ring buffer between lz4_read and write pos, in which
         // case we need to copy byte by byte.
         if ((int)ringbuf->ringbuf_pos < ringbuf_copy_pos || 
             (int)ringbuf->ringbuf_pos > ringbuf_copy_pos+7) {
@@ -67,7 +73,7 @@ void __ringbuf_copy(decoder_ringbuf* ringbuf, int copy_offset, unsigned char* ds
 
         // Finish copying the remaining bytes
         while (wn > 0) {
-            unsigned char value = ringbuf->ringbuf[ringbuf_copy_pos];
+            uint8_t value = ringbuf->ringbuf[ringbuf_copy_pos];
             dst[dst_pos] = value;
             ringbuf->ringbuf[ringbuf->ringbuf_pos] = value;
 
@@ -87,23 +93,25 @@ void __ringbuf_copy(decoder_ringbuf* ringbuf, int copy_offset, unsigned char* ds
 #define LZ4_LITERALS_RUN_LEN 15
 #define LZ4_MATCH_RUN_LEN 15
 #define LZ4_MIN_MATCH_SIZE 4
-#define HEADER_SIZE 16
+#define LZ4_EOF 0
+#define HEADER_SIZE 8
 
-typedef struct decoder {
-    unsigned char buf[LZ4_BUFFER_SIZE] __attribute__((aligned(8)));
-    unsigned char* src;
-    size_t bufIndex;
-    size_t bufSize;
-    bool isEOF;
-    size_t srcSize;
-    size_t readBytes;
-
-    unsigned char token;
-    unsigned char state;
+typedef struct decoder_faststate {
+    uint8_t token;
     int litLen;
     int matchLen;
     int matchOffset;
+    int state;
+} decoder_faststate;
 
+typedef struct decoder {
+    uint8_t buf[LZ4_BUFFER_SIZE] __attribute__((aligned(8)));
+    uint8_t* src;
+    uint8_t* srcEnd;
+    int bufIndex;
+    int bufSize;
+    bool isEOF;
+    decoder_faststate fastState;
     decoder_ringbuf ringbuf;
 } decoder;
 
@@ -114,125 +122,130 @@ enum decState {
     STATE_MAX
 };
 
-size_t romRead(unsigned char* buf, size_t size, unsigned char* src) {
-    size_t readBytes = size;
-    DMARomToRam(src, buf, size);
-    return readBytes;
+size_t lz4_fread(decoder* lz4) {
+    size_t i;
+
+    if (lz4->src == lz4->srcEnd)
+        return LZ4_EOF;
+
+    i = (lz4->src + LZ4_BUFFER_SIZE) > lz4->srcEnd ? (lz4->srcEnd - lz4->src) : LZ4_BUFFER_SIZE;
+    DMARomToRam(lz4->src, lz4->buf, i);
+    lz4->src += i;
+    assert(lz4->src <= lz4->srcEnd);
+    return i;
 }
 
-static void refill(decoder* lz4) {
-    lz4->readBytes = romRead(lz4->buf, sizeof(lz4->buf), lz4->src);
-    lz4->src += lz4->readBytes;
-    lz4->bufSize = lz4->readBytes;
+static void lz4_refill(decoder* lz4) {
+    lz4->bufSize = lz4_fread(lz4);
     lz4->bufIndex = 0;
-    lz4->isEOF = (lz4->readBytes > lz4->srcSize);
+    lz4->isEOF = (lz4->bufSize == LZ4_EOF);
 }
 
-static unsigned char readbyte(decoder* lz4) {
-    if (lz4->bufIndex >= lz4->bufSize) {
-        refill(lz4);
-    }
+static uint8_t lz4_readbyte(decoder* lz4) {
+    if (lz4->bufIndex >= lz4->bufSize)
+        lz4_refill(lz4);
     return lz4->buf[lz4->bufIndex++];
 }
 
-static void read(decoder* lz4, void* buf, size_t len) {
-    while (len > 0) {
-        int n = MIN(len, lz4->bufSize - lz4->bufIndex);
+static void lz4_read(decoder* lz4, void* buf, size_t len) {
+    while (!lz4->isEOF && len > 0) {
+        int n = MIN(len, (size_t)(lz4->bufSize - lz4->bufIndex));
         memcpy(buf, lz4->buf + lz4->bufIndex, n);
         buf += n;
         len -= n;
         lz4->bufIndex += n;
-        if (lz4->bufIndex >= lz4->bufSize) {
-            refill(lz4);
-        }
+        if (lz4->bufIndex >= lz4->bufSize)
+            lz4_refill(lz4);
     }
 }
 
-void lz4_init(decoder* lz4, unsigned char* src, size_t srcSize) {
+void lz4_init(decoder* lz4, uint8_t* src, size_t srcSize) {
     lz4->src = src;
+    lz4->srcEnd = src + srcSize;
     lz4->isEOF = false;
     lz4->bufIndex = 0;
     lz4->bufSize = 0;
-    lz4->srcSize = srcSize;
-    lz4->readBytes = 0;
 
-    lz4->token = 0;
-    lz4->state = STATE_READ_TOKEN;
-    lz4->litLen = 0;
-    lz4->matchLen = 0;
-    lz4->matchOffset = 0;
-
+    memset(&lz4->fastState, 0, sizeof(lz4->fastState));
     __ringbuf_init(&lz4->ringbuf);
 }
 
-ssize_t lz4_read(decoder* lz4, void* buf, size_t len) {
+ssize_t lz4_decompress(decoder* lz4, void* buf, size_t len) {
+    decoder_faststate st = lz4->fastState;
     void* buf_orig = buf;
     int n;
 
 	while (!lz4->isEOF && len > 0) {
-        switch (lz4->state) {
+        switch (st.state) {
             case STATE_READ_TOKEN:
-                lz4->token = readbyte(lz4);
-                lz4->litLen = ((lz4->token & 0xF0) >> 4);
-                if(unlikely(lz4->litLen == LZ4_LITERALS_RUN_LEN)) {
-                    unsigned char byte;
+                st.token = lz4_readbyte(lz4);
+                st.litLen = ((st.token & 0xF0) >> 4);
+                if(unlikely(st.litLen == LZ4_LITERALS_RUN_LEN)) {
+                    uint8_t byte;
                     do {
-                        byte = readbyte(lz4);
-                        lz4->litLen += byte;
-                    } while (unlikely(byte == 255));
+                        byte = lz4_readbyte(lz4);
+                        st.litLen += byte;
+                    } while (unlikely(byte == 255) && !lz4->isEOF);
                 }
-                lz4->state = STATE_LITERALS;
+                st.state = STATE_LITERALS;
                 FALLTHROUGH;
             case STATE_LITERALS:
-                n = MIN(lz4->litLen, (long)len);
-                read(lz4, buf, n);
+                n = MIN(st.litLen, (int)len);
+                lz4_read(lz4, buf, n);
                 __ringbuf_write(&lz4->ringbuf, buf, n);
                 buf += n;
                 len -= n;
-                lz4->litLen -= n;
-                if (lz4->litLen) {
+                st.litLen -= n;
+                if (st.litLen) {
                     break;
                 }
-                lz4->matchOffset = readbyte(lz4);
-                lz4->matchOffset |= ((unsigned short)readbyte(lz4) << 8);
-                lz4->matchLen = (lz4->token & 0x0F);
-                if(unlikely(lz4->matchLen == LZ4_MATCH_RUN_LEN)) {
-                    unsigned char byte;
+                st.matchOffset = lz4_readbyte(lz4);
+                st.matchOffset |= ((uint16_t)lz4_readbyte(lz4)) << 8;
+                st.matchLen = (st.token & 0x0F);
+                if(unlikely(st.matchLen == LZ4_MATCH_RUN_LEN)) {
+                    uint8_t byte;
                     do {
-                        byte = readbyte(lz4);
-                        lz4->matchLen += byte;
-                    } while (unlikely(byte == 255));
+                        byte = lz4_readbyte(lz4);
+                        st.matchLen += byte;
+                    } while (unlikely(byte == 255) && !lz4->isEOF);
                 }
-                lz4->matchLen += LZ4_MIN_MATCH_SIZE;
-                lz4->state = STATE_MATCH;
+                st.matchLen += LZ4_MIN_MATCH_SIZE;
+                st.state = STATE_MATCH;
                 FALLTHROUGH;
             case STATE_MATCH:
-                n = MIN(lz4->matchLen, (long)len);
-                __ringbuf_copy(&lz4->ringbuf, lz4->matchOffset, buf, n);
+                n = MIN(st.matchLen, (int)len);
+                __ringbuf_copy(&lz4->ringbuf, st.matchOffset, buf, n);
                 buf += n;
                 len -= n;
-                lz4->matchLen -= n;
-                if (lz4->matchLen) {
+                st.matchLen -= n;
+                if (st.matchLen) {
                     break;
                 }
-                lz4->state = STATE_READ_TOKEN;
+                st.state = STATE_READ_TOKEN;
         }
     }
 
+    lz4->fastState = st;
     return buf - buf_orig;
 }
 
 /*** Main ***/
 static decoder lz4;
 
-unsigned int getSizeFromHeader(unsigned char* sizePtr) {
+unsigned int getSizeFromHeader(uint8_t* sizePtr) {
     return (sizePtr[0] << 24) | (sizePtr[1] << 16) | (sizePtr[2] << 8) | (sizePtr[3] << 0);
 }
 
 size_t lz4dec(void *src, void *dst, size_t sz) {
-    unsigned char* _src = src;
+    uint8_t* _src = src;
     size_t decSize = getSizeFromHeader(_src + 4);
-    lz4_init(&lz4, _src + HEADER_SIZE, sz);
-    assert(lz4_read(&lz4, dst, sz) == (ssize_t)sz);
+
+    _src += HEADER_SIZE;
+    sz -= HEADER_SIZE;
+
+    lz4_init(&lz4, _src, sz);
+    ssize_t result = lz4_decompress(&lz4, dst, sz);
+    assert(result == (ssize_t)sz);
+
     return decSize;
 }
